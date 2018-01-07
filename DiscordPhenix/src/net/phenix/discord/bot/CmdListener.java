c649783662107bdce55b499ebce9804436a1eaa6
Nino <ninogre.pc@gmail.com>
2018-01-07 22:26:37 +0100

    Clean Code

--- DiscordPhenix/src/net/phenix/discord/bot/CmdListener.java
+++ DiscordPhenix/src/net/phenix/discord/bot/CmdListener.java
@@ -1,9 +1,11 @@
+package net.phenix.discord.bot;
+
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-public class MyListener extends ListenerAdapter 
+public class CmdListener extends ListenerAdapter 
 {
     @Override
     public void onMessageReceived(MessageReceivedEvent event)
--- DiscordPhenix/src/net/phenix/discord/bot/MainBot.java
+++ DiscordPhenix/src/net/phenix/discord/bot/MainBot.java
@@ -1,3 +1,4 @@
+package net.phenix.discord.bot;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
@@ -22,7 +23,7 @@ public class MainBot {
 		System.out.println("Connecte avec: " + jda.getSelfUser());
 		int i;
 		System.out.println("Le bot est autorisé sur " + (i = jda.getGuilds().size()) + " serveur" + (i > 1 ? "s" : ""));
-		jda.addEventListener(new MyListener());
+		jda.addEventListener(new CmdListener());
 	}
 
 	public static void main(String[] args) {
@@ -30,6 +31,11 @@ public class MainBot {
 			System.out.println("Veuillez indiquer le token du bot");
 		}
 
-		new MainBot("Mzk5NTk5MDYyODA4MjY0NzA0.DTPcqw.YrtSHrU7mITE2Z95dJ2E5tbpyd4");
+		//Prod : Mzk5NTk5MDYyODA4MjY0NzA0.DTPcqw.YrtSHrU7mITE2Z95dJ2E5tbpyd4
+		//Dev : Mzk5NjcxNjE4MTE0Mjg5Njc4.DTQhfw.3M1c4CE6Tniz7w0rDQ9QWbRv48Y
+		if(args[0] == null || ((String)args[0]).isEmpty()){
+			System.out.println("Veuillez renseigné le token.");
+		}
+		new MainBot(args[0]);
 	}
 }
